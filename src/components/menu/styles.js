8a47a192fbab90733d95c958b5e666023d7be15b
maramuraDev <maramuraDev@gmail.com>
2024-01-31 16:04:05 +0100

    Menu

--- src/components/menu/styles.js
+++ src/components/menu/styles.js
@@ -41,7 +41,7 @@ const MenuListItem = styled.li`
 const RollText = styled.span`
 	display: block;
 	position: relative;
-	transition: all 420ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+	transition: margin 420ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
 `;
 
 const MenuLink = styled(NavLink)`
