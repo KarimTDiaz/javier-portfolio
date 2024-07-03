f88f36a2be9aacb7f1b19fd4eb968457c6d7a763
maramuraDev <maramuraDev@gmail.com>
2024-07-03 19:44:06 +0100

    chore(assets-flaring): :memo: add assets
    now assets are smaller

--- src/components/project-image/ProjectImage.jsx
+++ src/components/project-image/ProjectImage.jsx
@@ -4,7 +4,7 @@ import { StyledImage } from './styles';
 
 const ProjectImage = ({ src }) => {
 	const ref = useRef(null);
-	const isInView = useInView(ref);
+	const isInView = useInView(ref, { once: true });
 
 	useEffect(() => {
 		console.log(isInView);
@@ -16,9 +16,9 @@ const ProjectImage = ({ src }) => {
 			src={src}
 			alt='Imagen de Proyecto'
 			style={{
-				transform: isInView ? 'none' : 'translateX(-200px)',
+				transform: isInView ? 'none' : 'translateY(-200px)',
 				opacity: isInView ? 1 : 0,
-				transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s'
+				transition: 'all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s'
 			}}
 		/>
 	);
--- src/constants/works.js
+++ src/constants/works.js
@@ -173,18 +173,15 @@ export const WORKS_ITEMS = [
 			'"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"'
 		],
 		images: [
-			'/assets/images/flaring/1-3.jpg',
-			'/assets/images/flaring/1-6.jpg',
-			'/assets/images/flaring/2-2.jpg',
-			'/assets/images/flaring/2-5.jpg',
-			'/assets/images/flaring/3-1.jpg',
-			'/assets/images/flaring/3-4.jpg',
-			'/assets/images/flaring/4-1.jpg',
-			'/assets/images/flaring/5-2.jpg',
-			'/assets/images/flaring/6-3.jpg',
-			'/assets/images/flaring/7-7.jpg',
-			'/assets/images/flaring/8-8.jpg',
-			'/assets/images/flaring/9-9.jpg'
+			'/assets/images/flaring/1.jpg',
+			'/assets/images/flaring/2.jpg',
+			'/assets/images/flaring/3.jpg',
+			'/assets/images/flaring/4.jpg',
+			'/assets/images/flaring/5.jpg',
+			'/assets/images/flaring/6.jpg',
+			'/assets/images/flaring/7.jpg',
+			'/assets/images/flaring/8.jpg',
+			'/assets/images/flaring/9.jpg'
 		]
 	},
 	{
