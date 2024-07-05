557cf2bd54964e93f606845bc9e521f77f2f1f44
maramuraDev <maramuraDev@gmail.com>
2024-07-05 17:27:05 +0100

    perf(project images): :zap: refactor and better made project image animation

--- src/components/project-image/ProjectImage.jsx
+++ src/components/project-image/ProjectImage.jsx
@@ -1,14 +1,16 @@
 import { useInView } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { StyledImage } from './styles';
 
 const ProjectImage = ({ src }) => {
 	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true });
-
-	useEffect(() => {
-		console.log(isInView);
-	}, [isInView]);
+	const isInView = useInView(
+		ref,
+		{ once: true },
+		{
+			margin: '0px 100px -500px 0px'
+		}
+	);
 
 	return (
 		<StyledImage
@@ -18,7 +20,7 @@ const ProjectImage = ({ src }) => {
 			style={{
 				transform: isInView ? 'none' : 'translateY(-200px)',
 				opacity: isInView ? 1 : 0,
-				transition: 'all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s'
+				transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s'
 			}}
 		/>
 	);
