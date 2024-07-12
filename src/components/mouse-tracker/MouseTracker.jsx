55ddbda42aa6587a459ecf91129d53d0c46c2ea0
maramuraDev <maramuraDev@gmail.com>
2024-07-12 22:21:04 +0100

    perf(MousdeTracker): :zap: improve mousetracker

--- src/components/mouse-tracker/MouseTracker.jsx
+++ src/components/mouse-tracker/MouseTracker.jsx
@@ -6,6 +6,7 @@ import {
 	useTransform
 } from 'framer-motion';
 import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
 import { COLORS } from '../../constants/variables';
 
 export const MouseImageTracker = ({ src }) => {
@@ -35,37 +36,42 @@ export const MouseImageTracker = ({ src }) => {
 	};
 
 	return (
-		<div
-			style={{
-				position: 'sticky',
-				top: '0',
-				width: '80vw',
-				marginLeft: 'auto',
-				marginRight: 'auto',
-				overflow: 'hidden',
-				/* display: 'flex', */
-				/* placeItems: 'center',
-				placeContent: 'center', */
-				border: `250px solid ${COLORS.primary}`,
-				borderRight: `275px solid ${COLORS.primary}`,
-				borderLeft: `275px solid ${COLORS.primary}`,
-				perspective: 400
-			}}
-			onMouseMove={handleMouse}
-		>
+		<TrackerWrapper onMouseMove={handleMouse}>
 			<motion.img
 				{...src}
 				style={{
 					position: 'sticky',
 					top: '0',
 					width: '100%',
-					/* height: '100vh', */
 					rotateX,
 					rotateY,
 					scale,
 					opacity
 				}}
 			/>
-		</div>
+		</TrackerWrapper>
 	);
 };
+
+const TrackerWrapper = styled.div`
+	position: sticky;
+	top: 15rem;
+	width: 90vw;
+	padding-top: 4rem;
+	margin-left: auto;
+	margin-right: auto;
+	overflow: hidden;
+	perspective: 400;
+	@media screen and (min-width: 640px) {
+	}
+	@media screen and (min-width: 1024px) {
+	}
+	@media screen and (min-width: 1440px) {
+		top: 0;
+		width: 80vw;
+		border: 80px solid ${COLORS.primary};
+		border-right: 275px solid ${COLORS.primary};
+		border-left: 275px solid ${COLORS.primary};
+		perspective: 400;
+	}
+`;
